7eb911c3ca9f856e1be011fcf768267b7d6c7e9c
Gabriel <gabriel@xmatch.dev>
2026-08-31 19:36:29 -0300

    fix: rota excluir sala

--- backend/src/sala/sala.controller.ts
+++ backend/src/sala/sala.controller.ts
@@ -47,11 +47,13 @@ export class SalaController {
   entrar(@Req() req: RequestComUsuario, @Body() dto: EntrarSalaDto) {
     return this.salaService.entrarComCodigo(req.user.id, dto);
   }
+  
   @Post(':id/sair')
   sair(@Req() req: RequestComUsuario, @Param('id') id: string) {
     return this.salaService.sair(req.user.id, id);
   }
 
+  @Post(':id/excluir')
   excluirSala(@Req() req: RequestComUsuario, @Param('id') id: string) {
     return this.salaService.excluir(req.user.id, id);
   }
